e3bfdec6445cdeaefcb83d2095fba9a9fd503e84
Bohdan <Bohdan@DESKTOP-PIR5C7D>
2023-10-26 15:57:42 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication21/ConsoleApplication21.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int factorial(int num)
+{
+    int fact = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i;
+    }
+    return fact;
+}
+
+int main()
+{
+    int num;
+    std::cout << "Enter the number: " << std::endl;
+    std::cin >> num;
+
+    if (num < 0)
+    {
+        std::cout << "Number can`t be less than 0";
+        return 0;
+    }
+
+    std::cout << "Factorial of a number " << num << " = " << factorial(num);
+}
